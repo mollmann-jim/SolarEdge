00a91e66bfba3ef977bb26213e02fe30bb7f4c4b
Jim Mollmann <jim.mollmann@gmail.com>
2026-03-17 09:40:46 -0400

    Add HOSTNAME and date header line to report. Only run report on NMB PC and Mac if not already run

--- collectDaily.sh
+++ collectDaily.sh
@@ -3,7 +3,7 @@
 logDir="$HOME/tools/SolarEdge/logs"
 log=$logDir/collectDaily.$(/bin/date +%F-%T | /usr/bin/tr : .);
 $HOME/tools/SolarEdge/collectDaily.py > $log 2>&1
-cat $log
+#cat $log
 # keep only the newest
 REMOVE=$(ls -t $logDir/collectDaily* | sed 1,20d)
 if [ -n "$REMOVE" ]; then
--- report.sh
+++ report.sh
@@ -3,10 +3,19 @@
 logDir="$HOME/tools/SolarEdge/logs"
 log=$logDir/report.$(/bin/date +%F-%T | /usr/bin/tr : .);
 reportDir="$HOME/SynologyDrive/Reports.Daily/"
-$HOME/tools/SolarEdge/report.py > $log 2>&1
+if [[ "$HOSTNAME" != "jim4" ]]; then
+    newAge=77
+    updated=$(find $HOME/SynologyDrive/Reports.Daily/ -name SolarEdge.txt -mmin -$newAge | wc -l)
+    if [[ $updated > 0 ]]; then
+	#echo already run
+	exit 0
+    fi
+fi
+echo -e "--------- $HOSTNAME --------- $(date) ----------\n" > $log
+$HOME/tools/SolarEdge/report.py >> $log 2>&1
 cp -p $log $reportDir/SolarEdge.txt
 cp -p $log $reportDir/All/SolarEdge.$(basename -- "$log").txt
-cat $log
+#cat $log
 # keep only the newest
 REMOVE=$(ls -t $logDir/report* | sed 1,20d)
 if [ -n "$REMOVE" ]; then
