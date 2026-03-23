85d3f43636600df9dbd2a5a69340031b8d9daa87
Jim Mollmann <jim.mollmann@gmail.com>
2026-03-23 13:33:59 -0400

    Add markup to set monospace build filenames as variables

--- report.sh
+++ report.sh
@@ -3,19 +3,22 @@
 logDir="$HOME/tools/SolarEdge/logs"
 log=$logDir/report.$(/bin/date +%F-%T | /usr/bin/tr : .);
 reportDir="$HOME/SynologyDrive/Reports.Daily/"
+reportName="SolarEdge"
 if [[ "$HOSTNAME" != "jim4" ]]; then
     newAge=77
-    updated=$(find $HOME/SynologyDrive/Reports.Daily/ -name SolarEdge.txt -mmin -$newAge | wc -l)
+    updated=$(find $HOME/SynologyDrive/Reports.Daily/ -name $reportName.txt -mmin -$newAge | wc -l)
     if [[ $(($updated + 0 ))  > 0 ]]; then
 	#echo already run
 	exit 0
     fi
 fi
-echo -e "--------- $HOSTNAME --------- $(date) ----------\n" > $log
+echo -e "------- $HOSTNAME --------- $(date) --------\n" > $log
 $HOME/tools/SolarEdge/report.py >> $log 2>&1
-cp -p $log $reportDir/SolarEdge.txt
-cp -p $log $reportDir/All/SolarEdge.$(basename -- "$log").txt
-#cat $log
+reportFile="$reportDir/$reportName.txt"
+cat $log | sed '1s/^/<div><tt>\n/g' | sed '$sZ$Z\n</tt></div>Zg' > $reportFile
+reportHistory="$reportDir/All/$reportName.$(basename -- "$log").txt"
+cp $reportFile $reportHistory
+
 # keep only the newest
 REMOVE=$(ls -t $logDir/report* | sed 1,20d)
 if [ -n "$REMOVE" ]; then
